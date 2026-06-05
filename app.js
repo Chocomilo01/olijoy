6f4ff9ccd887bed0676a33417a3a2396dee123a6
Chocomilo01 <kenechiodoh@gmail.com>
2026-06-05 09:02:29 +0100

    render

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://olijoy.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: [
