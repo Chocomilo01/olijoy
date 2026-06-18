869ece83f17f8e4f68fd7490baf294e5e83c633f
Chocomilo01 <kenechiodoh@gmail.com>
2026-06-18 04:22:34 +0100

    feat

--- services/smsService.js
+++ services/smsService.js
@@ -5,12 +5,10 @@ const formatPhone = (phone) => {
 
   phone = phone.toString().trim();
 
-  // +2349031234567 => 2349031234567
   if (phone.startsWith("+234")) {
     return phone.replace("+", "");
   }
 
-  // 09031234567 => 2349031234567
   if (phone.startsWith("0")) {
     return "234" + phone.slice(1);
   }
@@ -22,23 +20,24 @@ const sendSMS = async (phone, message) => {
   try {
     const formattedPhone = formatPhone(phone);
 
-    const response = await axios.post(
-      "https://api.ng.termii.com/api/sms/send",
+    const response = await axios.get(
+      "https://www.bulksmsnigeria.com/api/v1/sms/create",
       {
-        api_key: process.env.TERMII_API_KEY,
-        to: formattedPhone,
-        from: process.env.TERMII_SENDER_ID,
-        sms: message,
-        type: "plain",
-        channel: "generic",
+        params: {
+          api_token: process.env.BULKSMS_API_TOKEN,
+          from: process.env.BULKSMS_SENDER_ID,
+          to: formattedPhone,
+          body: message,
+          dnd: 2,
+        },
       },
     );
 
-    console.log("TERMII RESPONSE:", response.data);
+    console.log("BULKSMS RESPONSE:", response.data);
 
     return response.data;
   } catch (error) {
-    console.error("TERMII ERROR:", error.response?.data || error.message);
+    console.error("BULKSMS ERROR:", error.response?.data || error.message);
 
     return null;
   }
